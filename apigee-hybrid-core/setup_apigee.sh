e95758bcdda60e94e85109b9f44552c3002269a1
Rajesh Mishra <rajeshmi@google.com>
2025-06-03 09:53:22 -0700

    Waiting for K8

--- apigee-hybrid-core/setup_apigee.sh
+++ apigee-hybrid-core/setup_apigee.sh
@@ -205,6 +205,9 @@ install_crd() {
 
 install_cert_manager() {
     kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.16.3/cert-manager.yaml
+
+    #Wait for cert-manager to be ready
+    kubectl wait --for=condition=ready pod -l app=cert-manager -n cert-manager --timeout=120s
 }
 
 install_operator() {
@@ -218,6 +221,9 @@ install_operator() {
     --namespace $apigee_namespace \
     --atomic \
     -f $apigee_overrides_yaml_path
+
+    #Wait for operator to be ready
+    kubectl wait --for=condition=ready pod -l app=apigee-controller -n $apigee_namespace --timeout=120s
 }
 
 install_datastore() {
